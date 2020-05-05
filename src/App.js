17aa5ccf35e738c34fbf240b7547a25a9dd9e4c2
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-05 14:02:30 -0300

    [Form]  class for take all space enable label use htmlFor and not for acces input select--> for limit countries [Appjs] use class of materializre

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react'
 import Header from './components/Header'
+import Form from './components/Form'
 
 function App() {
   return (
     <Fragment>
-      <Header
-      title= 'Weather React app'/>
+      <Header title="Weather React app" />
 
+      <div className="contenedor-form">
+        <div className="conteiner">
+          <div className="row">
+            <div className="col m6 s12"><Form/></div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
     </Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
