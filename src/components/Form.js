b18601ebb563d38b225619395e8c5b534d3a53ed
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-05 14:39:24 -0300

    [Form] [useState] get info user selects and country Destructuring state search-> for use value city

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+  //State of Form
+  const [search, setSearch] = useState({
+      country: '',
+    city: '',
+  })
+
+  //Destructuration, extract city, country
+  const { country, city  } = search
+
+  //Handle info of click. Get info user select
+  const handleChange = (e) => {
+    //To update state
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    })
+  }
+
   return (
     <form>
       <div className="input-field col s12">
-        <input type="text" name="city" id="city" />
-        <label htmlFor="city"> City: </label>
+        <select
+          name="country"
+          id="country"
+          value={country}
+          onChange={handleChange}
+        >
+          <option value="">--Select a country--</option>
+          <option value="US">United States</option>
+          <option value="MX">Mexico</option>
+          <option value="AR">Argentina</option>
+          <option value="BR">Brazil</option>
+          <option value="CO">Colombia</option>
+          <option value="PE">Perú</option>
+          <option value="ES">España</option>
+        </select>
+        <label htmlFor="country"> Country: </label>
       </div>
 
       <div className="input-field col s12">
-        <select name="contry" id="country">
-          <option value="">--Select a country--</option>
-
-        </select>
-          <label htmlFor="country"> Country: </label>
+        <input
+          type="text"
+          name="city"
+          id="city"
+          value={city}
+          onChange={handleChange}
+        />
+        <label htmlFor="city"> City: </label>
       </div>
     </form>
   )
